22450c81094e04665e105cffb6c4fb10186a2c43
poojaunizen <52767282+poojaunizen@users.noreply.github.com>
2019-07-22 04:00:59 +0530

    Add files via upload

---
+++ First_Capstone_Project.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# # some markdowns
+# 
+# 1. Actual numbers don't matter, just that it's a number
+# 2. sub list
+# + Ordered sub-list
+# + another item
+# + Second item
+# 
+# [google home page](https://www.google.com)
+# 
+# [Google with title](https://www.google.com "Google's Homepage")
+# 
+# <dl>
+#   <dt>Definition list</dt>
+#   <dd>Is something people use sometimes.</dd>
+# 
+#   <dt>Markdown in HTML</dt>
+#   <dd>Does not work very well. Use HTML <em>tags</em>.</dd>
+# </dl>
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
